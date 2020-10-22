2c5f93d1b1593f500a94a43d97484651e64f4ff6
Feniks212 <Vovanchik211993@gmail.com>
2020-10-22 08:21:15 +0300

    fix js in home

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-unused-vars */
 import Swiper from 'swiper/swiper-bundle';
+import SimpleScrollbar from 'simple-scrollbar';
 
 export default {
   init() {
+
+    const body = document.querySelector('body');
+    const header = document.querySelector('.header');
 
     const option = {
       slidesPerView: 'auto',
@@ -19,6 +23,65 @@ export default {
       },
     }
     let mySwiper = new Swiper('.card-goss__slider.swiper-container', option);
+
+    const leftAside = document.querySelector('.aside-left');
+    const rightAside = document.querySelector('.aside-right');
+    const scrollDiv = document.querySelector('.scroll-div');
+
+    function positionSt() {
+      body.addEventListener('wheel', function(e) {
+
+        let meNow = false;
+
+        for (let i = 0; i < e.path.length - 3; i++) {
+          const element = e.path[i];
+          if (element) {
+            if (element.classList.contains('scroll-div')) {
+              meNow = true
+            }
+          }
+        }
+
+        if (!meNow) {
+          if (e.deltaY < 0) {
+            if (window.matchMedia('(max-width: 991px)').matches) {
+              leftAside.style.top = '';
+              rightAside.style.top = '';
+            } else {
+              leftAside.style.top = '90px';
+              rightAside.style.top = '90px';
+            }
+          } else {
+            if (window.matchMedia('(max-width: 991px)').matches) {
+              leftAside.style.top = '';
+              rightAside.style.top = '';
+            } else {
+              leftAside.style.top = '10px';
+              rightAside.style.top = '10px';
+            }
+          }
+        }
+      })
+    }
+
+    function heightWhat() {
+      let newHeightDiv = 0
+      newHeightDiv = (screen.height - header.clientHeight - 290);
+      console.log(newHeightDiv)
+      scrollDiv.style.height = newHeightDiv + 'px';
+    }
+
+    if (scrollDiv) {
+      SimpleScrollbar.initEl(scrollDiv);
+
+      heightWhat();
+    }
+
+    positionSt();
+    window.addEventListener('resize', function() {
+      positionSt();
+      heightWhat()
+    }, false);
   },
 
   // JavaScript to be fired on all pages, after page specific JS is fired

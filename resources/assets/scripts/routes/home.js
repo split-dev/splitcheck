c09d1cb8028ee345a9fbb8bb0c56120c432d3ce3
Darina <dbogunenko@splitdev.pro>
2020-10-22 23:19:13 +0300

    edits home

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -27,7 +27,9 @@ export default {
     const leftAside = document.querySelector('.aside-left');
     const rightAside = document.querySelector('.aside-right');
     const scrollDiv = document.querySelector('.scroll-div');
-
+    leftAside.addEventListener('scroll', function() {
+      leftAside.style.top = '-35px';
+    })
     function positionSt() {
       body.addEventListener('wheel', function(e) {
 

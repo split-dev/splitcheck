5eaa5401a1831568cea997cb005b1992f12c26d2
Feniks212 <Vovanchik211993@gmail.com>
2020-10-22 06:24:50 +0300

    fix js swiper

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -6,19 +6,19 @@ export default {
 
     const option = {
       slidesPerView: 'auto',
+      spaceBetween: 12,
       setWrapperSize: true,
       breakpoints: {
         767: {
           slidesPerView: 4,
-          simulateTouch: false,
           mousewheel: true,
+          setWrapperSize: false,
+          simulateTouch: false,
           // freeMode: true,
         },
       },
     }
     let mySwiper = new Swiper('.card-goss__slider.swiper-container', option);
-
-
   },
 
   // JavaScript to be fired on all pages, after page specific JS is fired

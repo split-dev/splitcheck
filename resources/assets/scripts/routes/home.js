889d2cb37f719f5c550a1e42e5450d0ac0c71cf8
Feniks212 <Vovanchik211993@gmail.com>
2020-11-11 18:31:22 +0200

    fix IE

--- resources/assets/scripts/routes/home.js
+++ resources/assets/scripts/routes/home.js
@@ -313,70 +313,70 @@ export default {
         thumbnailHeight: null,
         maxFiles: 1,
 
-        transformFile: function(file, done) {
-          let myDropZone = this;
+        // transformFile: function(file, done) {
+        //   let myDropZone = this;
 
-          // Create the image editor overlay
-          let editor = document.createElement('div');
-          editor.style.backgroundColor = '#e8ebf2';
-          editor.style.position = 'absolute';
-          editor.style.top = 0;
-          editor.style.bottom = 0;
-          editor.style.left = 0;
-          editor.style.right = 0;
-          editor.style.zIndex = 2;
-          this.previewsContainer.appendChild(editor)
-          // document.body.appendChild(editor);
+        //   // Create the image editor overlay
+        //   let editor = document.createElement('div');
+        //   editor.style.backgroundColor = '#e8ebf2';
+        //   editor.style.position = 'absolute';
+        //   editor.style.top = 0;
+        //   editor.style.bottom = 0;
+        //   editor.style.left = 0;
+        //   editor.style.right = 0;
+        //   editor.style.zIndex = 2;
+        //   this.previewsContainer.appendChild(editor)
+        //   // document.body.appendChild(editor);
 
-          // Create confirm button at the top left of the viewport
-          // let buttonConfirm = document.createElement('button');
-          // buttonConfirm.style.position = 'absolute';
-          // buttonConfirm.style.left = '10px';
-          // buttonConfirm.style.top = '10px';
-          // buttonConfirm.style.zIndex = 9999;
-          // buttonConfirm.textContent = 'Confirm';
-          // editor.appendChild(buttonConfirm);
+        //   // Create confirm button at the top left of the viewport
+        //   // let buttonConfirm = document.createElement('button');
+        //   // buttonConfirm.style.position = 'absolute';
+        //   // buttonConfirm.style.left = '10px';
+        //   // buttonConfirm.style.top = '10px';
+        //   // buttonConfirm.style.zIndex = 9999;
+        //   // buttonConfirm.textContent = 'Confirm';
+        //   // editor.appendChild(buttonConfirm);
 
-          btnCrop.addEventListener('click', function() {
-            // Get the canvas with image data from Cropper.js
-            let canvas = cropper.getCroppedCanvas({
-              width: 256,
-              height: 256,
-            });
+        //   btnCrop.addEventListener('click', function() {
+        //     // Get the canvas with image data from Cropper.js
+        //     let canvas = cropper.getCroppedCanvas({
+        //       width: 256,
+        //       height: 256,
+        //     });
 
-            // Turn the canvas into a Blob (file object without a name)
-            canvas.toBlob(function(blob) {
-              // Create a new Dropzone file thumbnail
-              myDropZone.createThumbnail(
-                blob,
-                myDropZone.options.thumbnailWidth,
-                myDropZone.options.thumbnailHeight,
-                myDropZone.options.thumbnailMethod,
-                false,
-                function(dataURL) {
+        //     // Turn the canvas into a Blob (file object without a name)
+        //     canvas.toBlob(function(blob) {
+        //       // Create a new Dropzone file thumbnail
+        //       myDropZone.createThumbnail(
+        //         blob,
+        //         myDropZone.options.thumbnailWidth,
+        //         myDropZone.options.thumbnailHeight,
+        //         myDropZone.options.thumbnailMethod,
+        //         false,
+        //         function(dataURL) {
 
-                  // Update the Dropzone file thumbnail
-                  myDropZone.emit('thumbnail', file, dataURL);
-                  // Return the file to Dropzone
-                  done(blob);
-              });
-            });
+        //           // Update the Dropzone file thumbnail
+        //           myDropZone.emit('thumbnail', file, dataURL);
+        //           // Return the file to Dropzone
+        //           done(blob);
+        //       });
+        //     });
 
-            // Remove the editor from the view
-            editor.parentNode.removeChild(editor);
-          });
+        //     // Remove the editor from the view
+        //     editor.parentNode.removeChild(editor);
+        //   });
 
-          // Create an image node for Cropper.js
-          let image = new Image();
-          image.src = URL.createObjectURL(file);
-          editor.appendChild(image);
+        //   // Create an image node for Cropper.js
+        //   let image = new Image();
+        //   image.src = URL.createObjectURL(file);
+        //   editor.appendChild(image);
 
-          // Create Cropper.js
-          let cropper = new Cropper(image, {
-            aspectRatio: 1,
-            viewMode: 1,
-          });
-        },
+        //   // Create Cropper.js
+        //   let cropper = new Cropper(image, {
+        //     aspectRatio: 1,
+        //     viewMode: 1,
+        //   });
+        // },
       });
     }
   },

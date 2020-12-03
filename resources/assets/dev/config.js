a55a2b7063e03393f56d723dfe8a8ef3c5548a04
Darina <dbogunenko@splitdev.pro>
2020-12-03 23:05:35 +0200

    modal menu add select

--- resources/assets/dev/config.js
+++ resources/assets/dev/config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { argv } = require('yargs');
 const merge = require('webpack-merge');
 
+
 const desire = require('./util/desire');
 
 const userConfig = merge(desire(`${__dirname}/../config`), desire(`${__dirname}/../config-local`));

0af1a3816fee43526f6670babb1212215bcbccca
Darina <dbogunenko@splitdev.pro>
2021-01-05 20:54:31 +0200

    groups

--- resources/assets/scripts/routes/groups.js
+++ resources/assets/scripts/routes/groups.js
@@ -3,7 +3,10 @@ import Swiper from 'swiper/swiper-bundle';
 export default {
   init() {
     
-
+    $('.groups__oval').click(function() {
+      $(this).toggleClass('active-oval-btn');
+    });
+    $('#phone').mask('+38 00 0000 000');
 
       
 

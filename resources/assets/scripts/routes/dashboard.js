0c4518724bf6b5c141e9568170722ca289d05e4d
split-dev <asamusenko@splitdev.pro>
2021-01-13 14:44:29 +0200

    fix

--- resources/assets/scripts/routes/dashboard.js
+++ resources/assets/scripts/routes/dashboard.js
@@ -235,6 +235,38 @@ export default {
         hoverReaction();
       }
     }
+
+
+
+    //scroll nav
+    if($('.nav-product').hasClass('nav-product')) {
+      var positions = [],
+	currentActive = null,
+	links = $('.scroll-to');
+
+  $('.anchor').each(function(){
+    positions.push({
+      top: $(this).position().top - 100,
+      a: links.filter('[href="#'+$(this).attr('id')+'"]'),
+    });
+  });
+
+  positions = positions.reverse();
+
+  $(window).on('scroll',function() {
+    var winTop = $(window).scrollTop();
+    for(var i = 0; i < positions.length; i++){
+      if(positions[i].top < winTop){
+        if(currentActive !== i){
+          currentActive = i;
+          links.removeClass('active');
+          positions[i].a.addClass('active');
+        }
+        break;
+      }
+    }
+  });
+    }
   },
 
   // JavaScript to be fired on all pages, after page specific JS is fired

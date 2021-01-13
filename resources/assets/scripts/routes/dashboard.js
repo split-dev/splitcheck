8cfc3bf91d98374282b1233862e2d098a3dcf1f0
Feniks212 <vovanchik211993@gmail.com>
2021-01-13 13:54:14 +0200

    fix js dashboard

--- resources/assets/scripts/routes/dashboard.js
+++ resources/assets/scripts/routes/dashboard.js
@@ -11,67 +11,69 @@ export default {
     const scrollDiv = document.querySelectorAll('.scroll-div');
 
     //* function for "adminMenu" position when scrolling, and calculate height
-    {
-      const checkAside = (param, any) => {
-        if (param) {
-          if (window.matchMedia('(max-width: 991px)').matches) {
-            param.style.top = '';
-          } else {
-            param.style.top = any + 'px';
+    if (adminMenu) {
+      {
+        const checkAside = (param, any) => {
+          if (param) {
+            if (window.matchMedia('(max-width: 991px)').matches) {
+              param.style.top = '';
+            } else {
+              param.style.top = any + 'px';
+            }
           }
         }
-      }
 
-      const positionSt = () => {
-        const maxPadding = 94;
-        const minPadding = 10;
+        const positionSt = () => {
+          const maxPadding = 94;
+          const minPadding = 10;
 
-        body.addEventListener('wheel', function(e) {
-          let meNow = false;
+          body.addEventListener('wheel', function(e) {
+            let meNow = false;
 
-          if (!window.matchMedia('(max-width: 991px)').matches) {
-            if (e.path) {
-              for (let i = 0; i < e.path.length - 3; i++) {
-                const element = e.path[i];
-                if (element) {
-                  if (element.classList.contains('scroll-div')) {
-                    meNow = true
+            if (!window.matchMedia('(max-width: 991px)').matches) {
+              if (e.path) {
+                for (let i = 0; i < e.path.length - 3; i++) {
+                  const element = e.path[i];
+                  if (element) {
+                    if (element.classList.contains('scroll-div')) {
+                      meNow = true
+                    }
                   }
                 }
               }
-            }
 
-            if (!meNow) {
-              let newHeightDiv = 0;
-              let newHeightadminMenuList = adminMenuList.offsetHeight;
+              if (!meNow) {
+                let newHeightDiv = 0;
+                let newHeightadminMenuList = adminMenuList.offsetHeight;
 
-              if (e.deltaY < 0) {
-                if (adminMenuNav) {
-                  checkAside(adminMenu, maxPadding);
-                  adminMenuNav.classList.remove('js-no-header')
+                if (e.deltaY < 0) {
+                  if (adminMenuNav) {
+                    checkAside(adminMenu, maxPadding);
+                    adminMenuNav.classList.remove('js-no-header')
+                  }
+                } else {
+                  if (adminMenuNav) {
+                    checkAside(adminMenu, minPadding);
+                    adminMenuNav.classList.add('js-no-header') // add class for height calculation
+                  }
                 }
-              } else {
-                if (adminMenuNav) {
-                  checkAside(adminMenu, minPadding);
-                  adminMenuNav.classList.add('js-no-header') // add class for height calculation
-                }
-              }
 
-              //* height calculation due to header being hidden
-              if (scrollDiv.length) {
-                for (let i = 0; i < scrollDiv.length; i++) {
-                  const el = scrollDiv[i];
-                  if (el.offsetParent) { //* height calculation
-                    newHeightDiv = (window.innerHeight - header.clientHeight - 20); // 20 - correction because of the shadows
-                    newHeightadminMenuList = adminMenuList.offsetHeight;
-                    if (el.offsetParent.classList.contains('admin-menu__nav')) {
-                      if ( (newHeightDiv - 80) > newHeightadminMenuList) {
-                        el.style.height = '';
-                      } else {
-                      if (el.offsetParent.classList.contains('js-no-header')) {
-                          el.style.height = (newHeightDiv - 15 ) + 'px'; // writing the height
+                //* height calculation due to header being hidden
+                if (scrollDiv.length) {
+                  for (let i = 0; i < scrollDiv.length; i++) {
+                    const el = scrollDiv[i];
+                    if (el.offsetParent) { //* height calculation
+                      newHeightDiv = (window.innerHeight - header.clientHeight - 20); // 20 - correction because of the shadows
+                      newHeightadminMenuList = adminMenuList.offsetHeight;
+                      if (el.offsetParent.classList.contains('admin-menu__nav')) {
+                        if ( (newHeightDiv - 80) > newHeightadminMenuList) {
+                          el.style.height = '';
                         } else {
-                          el.style.height = (newHeightDiv - 100) + 'px'; // writing the height // 120 - correction
+                        if (el.offsetParent.classList.contains('js-no-header')) {
+                            el.style.height = (newHeightDiv - 15 ) + 'px'; // writing the height
+                          } else {
+                            el.style.height = (newHeightDiv - 100) + 'px'; // writing the height // 120 - correction
+                          }
                         }
                       }
                     }
@@ -79,55 +81,55 @@ export default {
                 }
               }
             }
-          }
-        })
+          })
 
-        body.addEventListener('mousemove', function(e) {
-          let meNow = false;
-          if (e.path) {
+          body.addEventListener('mousemove', function(e) {
+            let meNow = false;
+            if (e.path) {
 
-            for (let i = 0; i < e.path.length - 3; i++) {
-              const element = e.path[i];
-              if (element) {
-                if (element.classList.contains('admin-menu')) {
-                  meNow = true
+              for (let i = 0; i < e.path.length - 3; i++) {
+                const element = e.path[i];
+                if (element) {
+                  if (element.classList.contains('admin-menu')) {
+                    meNow = true
 
-                  heightWhat();
+                    heightWhat();
+                  }
                 }
               }
             }
-          }
-          if (!meNow) return false
-        })
-      }
+            if (!meNow) return false
+          })
+        }
 
-      const heightWhat = () => {
-        // const modalHeaderHeight = 110; // default height Header in modal
-        let newHeightDiv = 0;
-        let newHeightadminMenuList = adminMenuList.offsetHeight;
+        const heightWhat = () => {
+          // const modalHeaderHeight = 110; // default height Header in modal
+          let newHeightDiv = 0;
+          let newHeightadminMenuList = adminMenuList.offsetHeight;
 
-        if (scrollDiv.length) {
-          for (let i = 0; i < scrollDiv.length; i++) {
-            const el = scrollDiv[i];
-            SimpleScrollbar.initEl(el);
-            if (el.offsetParent) { //* height calculation "aside-right" and "aside-left"
-              newHeightDiv = (window.innerHeight - header.clientHeight - 20); // 20 - correction because of the shadows
-              newHeightadminMenuList = adminMenuList.offsetHeight;
+          if (scrollDiv.length) {
+            for (let i = 0; i < scrollDiv.length; i++) {
+              const el = scrollDiv[i];
+              SimpleScrollbar.initEl(el);
+              if (el.offsetParent) { //* height calculation "aside-right" and "aside-left"
+                newHeightDiv = (window.innerHeight - header.clientHeight - 20); // 20 - correction because of the shadows
+                newHeightadminMenuList = adminMenuList.offsetHeight;
 
-              if (el.offsetParent.classList.contains('admin-menu__nav')) {
-                if (window.matchMedia('(max-width: 991px)').matches) {
-                  el.style.height = (newHeightDiv) + 'px'; // writing the height // 120 - correction
-                  // if (window.matchMedia('(max-width: 767px)').matches) {
-                  //   el.style.height = (newHeightDiv - 30) + 'px'; // writing the height // 120 - correction
-                  // }
-                } else {
-                  if ( (newHeightDiv - 80) > newHeightadminMenuList) {
-                    el.style.height = '';
+                if (el.offsetParent.classList.contains('admin-menu__nav')) {
+                  if (window.matchMedia('(max-width: 991px)').matches) {
+                    el.style.height = (newHeightDiv) + 'px'; // writing the height // 120 - correction
+                    // if (window.matchMedia('(max-width: 767px)').matches) {
+                    //   el.style.height = (newHeightDiv - 30) + 'px'; // writing the height // 120 - correction
+                    // }
                   } else {
-                    if (el.offsetParent.classList.contains('js-no-header')) {
-                      el.style.height = (newHeightDiv - 15 ) + 'px'; // writing the height
+                    if ( (newHeightDiv - 80) > newHeightadminMenuList) {
+                      el.style.height = '';
                     } else {
-                      el.style.height = (newHeightDiv - 100) + 'px'; // writing the height // 120 - correction
+                      if (el.offsetParent.classList.contains('js-no-header')) {
+                        el.style.height = (newHeightDiv - 15 ) + 'px'; // writing the height
+                      } else {
+                        el.style.height = (newHeightDiv - 100) + 'px'; // writing the height // 120 - correction
+                      }
                     }
                   }
                 }
@@ -135,17 +137,17 @@ export default {
             }
           }
         }
-      }
 
-      //* function init
-      heightWhat();
-      positionSt();
-
-      //* function resize
-      window.addEventListener('resize', function() {
+        //* function init
         heightWhat();
         positionSt();
-      }, false);
+
+        //* function resize
+        window.addEventListener('resize', function() {
+          heightWhat();
+          positionSt();
+        }, false);
+      }
     }
 
     //clone new box .add-field
@@ -173,7 +175,6 @@ export default {
         for (let i = 0; i < psItems.length; i++) {
           const element = psItems[i];
 
-          console.log(element)
           const ps = new PerfectScrollbar(element);
 
           window.onresize = resize(ps);
@@ -183,20 +184,22 @@ export default {
 
     //* mobile append aside right
     {
-      const tpCollection = () => {
-        if(window.innerWidth < 992) {
-          $('.section-aside-right-mob').append($('.aside-right .section-content'))
-        } else {
-          if($('.section-aside-right-mob .section-content').hasClass('section-content')) {
-            $('.aside-right').append($('.section-aside-right-mob .section-content'))
+      if($('.section-aside-right-mob').length > 0) {
+        const tpCollection = () => {
+          if(window.innerWidth < 992) {
+            $('.section-aside-right-mob').append($('.aside-right .section-content'))
+          } else {
+            if($('.section-aside-right-mob .section-content').hasClass('section-content')) {
+              $('.aside-right').append($('.section-aside-right-mob .section-content'))
+            }
           }
         }
-      }
 
-      tpCollection()
-      $(window).resize( function() {
-        tpCollection();
-      })
+        tpCollection()
+        $(window).resize( function() {
+          tpCollection();
+        })
+      }
     }
 
     //* function hover "reaction-btn-like"

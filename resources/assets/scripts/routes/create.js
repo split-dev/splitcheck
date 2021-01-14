3e396e16346ec6275d1f9e76642881c0e47d58c1
split-dev <asamusenko@splitdev.pro>
2021-01-14 10:44:26 +0200

    test drag

--- resources/assets/scripts/routes/create.js
+++ resources/assets/scripts/routes/create.js
@@ -1,3 +1,5 @@
+import Sortable from 'sortablejs/modular/sortable.core.esm';
+
 export default {
   init() {
       $('.images-box-loader ul li').click( function () {
@@ -14,6 +16,26 @@ export default {
         $('.product-type-box__body div').hide();
         $(data.id).show();
     });
+
+    //drag and drop image
+
+    if(document.getElementById('columns-sort-drag')) {
+      var elTwo = document.getElementById('columns-sort-drag');
+      /*eslint-disable no-unused-vars*/
+      var sortableTwo = Sortable.create(elTwo, {
+        filter: '.ignore-elements',
+        swap: true,
+        swapClass: 'highlighted',
+        animation: 100,
+        // Event when you move an item in the list or between lists
+      onMove: function (evt) {
+        if($(evt.related).hasClass('ignore-elements')) {
+          return false;
+        }
+      },
+      });
+    }
+    
   },
 
   // JavaScript to be fired on all pages, after page specific JS is fired
